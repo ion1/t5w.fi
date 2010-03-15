744a5783e154dbfad0645581c9d20db9439d40db
Johan Kiviniemi <devel@johan.kiviniemi.name>
2010-03-15 02:58:15 +0200

    Project: validate presence of attributes

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Project do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid with correct attributes" do
+    project = Factory.build :project
+    project.should be_valid
+  end
+
+  [:name, :what, :when, :why].each do |attribute|
+    it "should not be valid when attribute '#{attribute}' is blank" do
+      project = Factory.build :project, attribute => ''
+
+      project.should_not be_valid
+      project.errors[attribute].should include "can't be blank"
+      project.errors.count.should be 1
+    end
+  end
 end
